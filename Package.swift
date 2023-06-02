4ef57ed4898a6d853fe7b761d412c38a50c6a9bb
Roman Silin <roman.s@sumsub.com>
2023-06-02 17:55:23 +0300

    Release 1.25.0

--- Package.swift
+++ Package.swift
@@ -3,71 +3,82 @@
 
 import PackageDescription
 
-let version = "1.24.0"
+let version = "1.25.0"
+let gitUrl = "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)"
 
 enum checksums {
     
-    static let msdk = "277951881ccfb9e0148684b0c761d20104ca642dc4fc756877a2eb19df52d86b"
-    static let mrtd = "67823cc40714fd09be29e31b84c1fc261c9dae4ecccb8a86b7c7e9946186846d"
-    static let vi   = "d1d5548293daf7d177db2fc0ef871a98f5bd134d92f2e9ed287a7b898fd18164"
+    static let msdk = "70daae7c79c69e5599423437bf55c46cda5bb7cc7fdbe623d5bd9eea892a307e"
+    static let mrtd = "6996ec720a0bcea06ce0425a7c2f0270b6315a3075ed86fd86af583975079878"
+    static let vi   = "f9acc2456ce4a6c0c663bd0964c51025a7c0f937dfe6c95409353f24d20e37c1"
     
 }
 
 let package = Package(
     name: "IdensicMobileSDK",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v11)
     ],
     products: [
         .library(
             name: "IdensicMobileSDK",
-            targets: ["IdensicMobileSDK"]
+            targets: [
+                "IdensicMobileSDK"
+            ]
         ),
         .library(
             name: "IdensicMobileSDK_MRTDReader",
-            targets: ["IdensicMobileSDK", "MRTDReaderWrapper"]
+            targets: [
+                "IdensicMobileSDK", "MRTDReaderWrapper"
+            ]
         ),
         .library(
             name: "IdensicMobileSDK_VideoIdent",
-            targets: ["IdensicMobileSDK", "VideoIdentWrapper"]
+            targets: [
+                "IdensicMobileSDK", "VideoIdentWrapper"
+            ]
         )
     ],
     dependencies: [
         .package(
-            name: "OpenSSL",
-            url: "https://github.com/krzyzanowskim/OpenSSL.git",
+            url: "https://github.com/krzyzanowskim/OpenSSL",
             "1.0.0" ..< "2.0.0"
         ),
         .package(
-            name: "TwilioVideo",
-            url: "https://github.com/twilio/twilio-video-ios.git",
-            "4.0.0" ..< "6.0.0"
+            url: "https://github.com/twilio/twilio-video-ios",
+            "4.0.0" ..< "5.0.0"
         )
     ],
     targets: [
         .binaryTarget(
             name: "IdensicMobileSDK",
-            url: "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)/IdensicMobileSDK_Core-\(version).zip",
+            url: "\(gitUrl)/IdensicMobileSDK_Core-\(version).zip",
             checksum: checksums.msdk
         ),
         .binaryTarget(
             name: "IdensicMobileSDK_MRTDReader",
-            url: "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)/IdensicMobileSDK_MRTDReader-\(version).zip",
+            url: "\(gitUrl)/IdensicMobileSDK_MRTDReader-\(version).zip",
             checksum: checksums.mrtd
         ),
         .binaryTarget(
             name: "IdensicMobileSDK_VideoIdent",
-            url: "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)/IdensicMobileSDK_VideoIdent-\(version).zip",
+            url: "\(gitUrl)/IdensicMobileSDK_VideoIdent-\(version).zip",
             checksum: checksums.vi
         ),
         .target(
             name: "MRTDReaderWrapper",
-            dependencies: ["IdensicMobileSDK_MRTDReader", "OpenSSL"],
+            dependencies: [
+                "IdensicMobileSDK_MRTDReader",
+                "OpenSSL"
+            ],
             path: "MRTDReaderWrapper"
         ),
         .target(
             name: "VideoIdentWrapper",
-            dependencies: ["IdensicMobileSDK_VideoIdent", "TwilioVideo"],
+            dependencies: [
+                "IdensicMobileSDK_VideoIdent",
+                .product(name: "TwilioVideo", package: "twilio-video-ios")
+            ],
             path: "VideoIdentWrapper"
         )
     ]
