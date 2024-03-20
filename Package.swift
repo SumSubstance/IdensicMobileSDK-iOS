498e2a7501a77233181b859b77bd6904ed22aff8
Sergey Kokunov <sergress@gmail.com>
2024-03-20 18:17:05 +0300

    Release 1.30.0

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@
 
 import PackageDescription
 
-let version = "1.29.0"
+let version = "1.30.0"
 let gitUrl = "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)"
 
 enum checksums {
     
-    static let msdk = "ee360ab8a7154edd9580790adad02d8bf84efce3182f698f8ef8822ce00a0590"
-    static let mrtd = "35b6b8aa7bb93885f2129ed3f8318acee0ddca4fe7256c206742d65d3b14d549"
-    static let vi   = "60f2a1d672b7783baa2195fa68cecc1d25b02b94cfa886aef66ef71b739ad1d3"
+    static let msdk = "ff44cfa3de1e1f19e44a8fa13091cc19ba8de579a41432dcd80bfea4aefe72eb"
+    static let mrtd = "80ffbb8bb804a99c39ee69f1819273d5fce857f21f288e815673155d33f92773"
+    static let vi   = "7708198bc5b36cf871e963b7fca7fe3f084dc5508ca8e4022ffdeb95ca14bf7d"
 
 }
 
