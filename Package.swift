6f2cbea2363aba3553843c3715ad66e6f61a2b63
Alexey Nazarov <alexey.nazarov@sumsub.com>
2026-03-31 15:20:19 +0300

    Release 1.42.0

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 
 import PackageDescription
 
-let version = Version("1.41.1")
+let version = Version("1.42.0")
 
 let gitUrl = "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)"
 
 enum checksums {
-    static let msdk = "706f412fbdbabcab6529cc2ad9503ac81e632cb7c52ceea27d5781c0b4045ae4"
+    static let msdk = "04c700b67a9917ec1c93d11643597862c1daf6f307bfaf7ba9565574e9015fd1"
 }
 
 let package = Package(
